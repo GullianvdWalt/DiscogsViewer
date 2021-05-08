8255644e1dcb8de7c22e37ab80b4472e6e7a6b6a
Gullian <gullianvdwalt1@gmail.com>
2021-05-08 09:26:30 +0200

    init

---
+++ src/main/java/com/gvdw/discogsviewer/DiscogsViewerApplication.java
@@ -0,0 +1,13 @@
+package com.gvdw.discogsviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscogsViewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscogsViewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gvdw/discogsviewer/DiscogsViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvdw.discogsviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscogsViewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
